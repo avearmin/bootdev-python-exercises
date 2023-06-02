eb04ff2dcc1115a9e028f8656b6bb19b93746efb
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-02 14:25:09 -0400

    Add starter code for counting potions in inventory
    - Course: Learn Data Structures
    - Chapter: 1. Introduction
    - Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/a9d59658-4e3c-441e-973b-147cc3c7e9de/d2176e10-f96b-4f57-af5c-e03e5c46fc88
    
    In this commit, I introduce the initial code and exercise link for
    counting the occurrences of Healing potions in a player's inventory. The
    objective is to implement the count_potions function that takes a list
    of inventory items (strings) as input and returns the number of times
    the 'Healing potion' appears in the list as an integer. This exercise is
    part of the "Learn Data Structures" course, specifically Chapter 1. The
    task of managing inventory items aligns with simulating the tracking of
    items within a larger system or application.

---
+++ learn-data-structures/introduction/count_potions.py
@@ -0,0 +1,43 @@
+# exercise link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/a9d59658-4e3c-441e-973b-147cc3c7e9de/d2176e10-f96b-4f57-af5c-e03e5c46fc88
+
+import random
+
+
+def count_potions(inventory):
+    # ?
+
+
+# don't touch below this line
+
+
+def get_inventory(num):
+    random.seed(1)
+    options = [
+        "Short sword",
+        "Bread",
+        "Healing potion",
+        "Leather scraps",
+        "Chainmail Armor",
+    ]
+    inventory = []
+    for i in range(num):
+        optionI = random.randint(0, len(options) - 1)
+        inventory.append(options[optionI])
+    return inventory
+
+
+def test(num):
+    inventory = get_inventory(num)
+    print(f"Total items in Inventory: {num}")
+    print(f"Number of potions: {count_potions(inventory)}")
+    print("=================================")
+
+
+def main():
+    test(10)
+    test(100)
+    test(1000)
+    test(10000)
+
+
+main()
