aa6f7f66962efe719d9c81127e840be2f35d27a9
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-02 14:44:26 -0400

    Implement solution for count_potions exercise
    In this commit, I provide the solution for the count_potions exercise.
    The solution iterates through the inventory list and increments a count
    variable whenever a "Healing potion" is encountered. The final count is
    returned, effectively counting the occurrences of "Healing potion" in
    the inventory.

--- learn-data-structures/introduction/count_potions.py
+++ learn-data-structures/introduction/count_potions.py
@@ -4,7 +4,11 @@ import random
 
 
 def count_potions(inventory):
-    # ?
+    count = 0
+    for item in inventory:
+        if item == "Healing potion":
+            count += 1
+    return count
 
 
 # don't touch below this line
