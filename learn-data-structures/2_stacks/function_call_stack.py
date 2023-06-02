512d53642c4f2a982a37349193ca4814615f77e7
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-02 19:50:15 -0400

    Add starter code for function call stack exercise
    Course: Learn Data Structures
    Chapter: 2. Stacks
    Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/61362b33-a3cc-4ad7-b27c-a2dfaf6cda48/a22849d4-4da2-497b-84c7-7d0145cbc893
    
    In this commit, I provide the initial starter code for the exercise
    focused on visualizing the function call stack. The code is implemented
    in a new file called 'function_call_stack.py' within the '2_stacks' directory.
    The file includes three functions: 'attack_action', 'shoot_arrow', and
    'calc_damage', which need to be modified as part of the exercise.
    
    The 'call' function is introduced to facilitate visualizing and tracking
    function calls. It pushes the name of the called function onto the
    stack, executes the function, and then pops the function name from the
    stack. The 'Stack' class is also included, providing basic stack
    operations.

---
+++ learn-data-structures/2_stacks/function_call_stack.py
@@ -0,0 +1,59 @@
+# exercise link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/61362b33-a3cc-4ad7-b27c-a2dfaf6cda48/a22849d4-4da2-497b-84c7-7d0145cbc893
+
+def attack_action():
+    # ?
+
+
+def shoot_arrow():
+    # ?
+
+
+def calc_damage():
+    # ?
+
+
+# don't touch below this line
+
+
+def calc_new_health():
+    pass
+
+
+def apply_damage():
+    pass
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        return self.items.pop()
+
+    def peek(self):
+        if len(self.items) == 0:
+            return None
+        return self.items[len(self.items) - 1]
+
+
+stack = Stack()
+
+
+def call(func):
+    stack.push(func.__name__)
+    print("Pushing " + func.__name__)
+    print("Stack: " + str(stack.items))
+    print("=================================")
+    func()
+    stack.pop()
+    print("Popping " + func.__name__)
+    print("Stack: " + str(stack.items))
+    print("=================================")
+
+
+call(attack_action)
