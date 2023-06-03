3546a8155c9f7e964344052efd7f721ef6f762af
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-02 20:12:23 -0400

    Implement solution for function call stack exercise

--- learn-data-structures/2_stacks/function_call_stack.py
+++ learn-data-structures/2_stacks/function_call_stack.py
@@ -1,15 +1,16 @@
 # exercise link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/61362b33-a3cc-4ad7-b27c-a2dfaf6cda48/a22849d4-4da2-497b-84c7-7d0145cbc893
 
 def attack_action():
-    # ?
+    call(shoot_arrow)
+    call(calc_new_health)
 
 
 def shoot_arrow():
-    # ?
+    call(calc_damage)
 
 
 def calc_damage():
-    # ?
+    call(apply_damage)
 
 
 # don't touch below this line
