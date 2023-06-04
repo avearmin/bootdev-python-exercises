074ea1bbba501148bcc174e7d7f3e115e4695b4f
avearmin <117543612+avearmin@users.noreply.github.com>
2023-06-04 16:10:39 -0400

    Add starter code for node class exercise
    Course: Learn Data Structures
    Chapter: 3. Queues
    Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/a96ca39e-3b99-4a7a-ad9b-37c8e1a61ff6/6d49ab0d-1c58-44ba-9a78-22f22178f1ba
    
    In this commit, I introduce the initial starter code for the node class 
    exercise. The code is implemented in a new file called 'node_class.py' 
    within the newly created '3_queues' directory. The assignment asks us 
    to complete the complete the constructor and the `set_next` methods
    in the Node class.

---
+++ learn-data-structures/3_queues/node_class.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, val):
+        # ?
+
+    def set_next(self, node):
+        # ?
+
+    # don't touch below this line
+
+    def __repr__(self):
+        return self.val
+
+
+def set_next_test(node_lhs, node_rhs):
+    node_lhs.set_next(node_rhs)
+    print(f"setting '{node_rhs}' as next of '{node_lhs}'")
+    print("=================================")
+
+
+def print_linked_node(node):
+    print(f" - {node}")
+    if node.next:
+        print_linked_node(node.next)
+
+
+def print_linked_list(node):
+    print(f"Linked list starting at '{node}':")
+    print_linked_node(node)
+    print("=================================")
+
+
+def main():
+    node0 = Node("Sword")
+    node1 = Node("Bow")
+    node2 = Node("Axe")
+    node3 = Node("Spear")
+    set_next_test(node0, node1)
+    set_next_test(node1, node2)
+    set_next_test(node2, node3)
+    print_linked_list(node0)
+
+    node4 = Node("Mace")
+    set_next_test(node3, node4)
+    print_linked_list(node0)
+
+    print_linked_list(node2)
+
+
+main()
