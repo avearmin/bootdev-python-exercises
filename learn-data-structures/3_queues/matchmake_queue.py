d74205cc1b6ef8066bac3d8d6c4903004d59e170
avearmin <117543612+avearmin@users.noreply.github.com>
2023-06-04 22:51:48 -0400

    Add starter code for matchmake queue exercise
    I have implemented starter code for the matchmake queue exercise, in a 
    new file named `matchmake_queue.py` in the `3_queue` directory. 
    
    The exercise tasks us to create a function `matchmaking_queue` that simulates 
    players joining and leaving the matchmaking queue. The function should take 
    a list of player names and their actions (either "join" or "leave") as a tuple.
    
    For each call to matchmake:
    1. If the action is "leave", search the queue for the player and remove them if they 
    are in the queue.
    2. If the action is "join", push the player onto the queue if they are not already in the 
    queue.
    3. After a push, check if the queue has at least 4 players in it. If so, pop the first 2 players 
    from the queue and print the following to the console: "{player1} matched {player2}!"
    Where player1 is the first player popped and player2 is the second player popped.

---
+++ learn-data-structures/3_queues/matchmake_queue.py
@@ -0,0 +1,84 @@
+# link to exercise: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/a96ca39e-3b99-4a7a-ad9b-37c8e1a61ff6/cd2268ff-f75b-4e02-ae47-148818efced1
+
+def matchmake(queue, player):
+    # ?
+
+
+# don't touch below this line
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.insert(0, item)
+
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        temp = self.items[-1]
+        del self.items[-1]
+        return temp
+
+    def peek(self):
+        if len(self.items) == 0:
+            return None
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def search_and_remove(self, item):
+        if item not in self.items:
+            return None
+        self.items.remove(item)
+        return item
+
+
+def test(players):
+    queue = Queue()
+    for player in players:
+        name = player[0]
+        action = player[1]
+        if action == "leave":
+            print(f"{name} left the queue.")
+        if action == "join":
+            print(f"{name} joined the queue.")
+        matchmake(queue, player)
+        print(f"Players in queue: {queue.items}")
+        print("-------------------------------------")
+    print("=====================================")
+
+
+def main():
+    test(
+        [
+            ("Alice", "join"),
+            ("Bob", "join"),
+            ("Charlie", "join"),
+            ("David", "join"),
+            ("Eve", "join"),
+            ("Frank", "join"),
+            ("Frank", "leave"),
+            ("Grace", "join"),
+        ]
+    )
+    test(
+        [
+            ("Frank", "join"),
+            ("Alice", "join"),
+            ("Bob", "join"),
+            ("Charlie", "leave"),
+            ("David", "join"),
+            ("Eve", "join"),
+            ("Errol", "join"),
+            ("Jake", "join"),
+            ("Errol", "leave"),
+            ("Errol", "leave"),
+            ("Grace", "join"),
+        ]
+    )
+
+
+main()
