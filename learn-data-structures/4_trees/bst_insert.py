5b26427b0c7a8233090810de856ed2ecf66edff2
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-06 19:55:06 -0400

    Add starter code for BST insert exercise
    Course: Learn Data Structures
    Chapter: 4. Trees
    Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/36c4edda-52be-4679-a7d4-8fd81c7f7bdb/8b49e869-e92f-472b-82ea-c845ef635f88
    
    This exercise asks us to implement a "find a friend" feature for a
    fictional game called Fantasy Quest. The friends list for Fantasy Quest
    is implemented as a binary search tree, where each node represents a
    `Character` object. The `Character` object contains a name and "gamer
    tag" (which is a unique number id).
    
    We are to complete the `insert` method, which takes a `Character` object
    as an input, and adds it to a new node if it doesnt't already exist in
    the tree.

---
+++ learn-data-structures/4_trees/bst_insert.py
@@ -0,0 +1,101 @@
+# exercise_link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/36c4edda-52be-4679-a7d4-8fd81c7f7bdb/8b49e869-e92f-472b-82ea-c845ef635f88
+
+import random
+
+
+class BSTNode:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        # ?
+
+    # don't touch below this line
+
+    def __repr__(self):
+        lines = []
+        print_tree(self, lines)
+        return "\n".join(lines)
+
+
+class Character:
+    def __init__(self, gamertag):
+        self.gamertag = gamertag
+        character_names = [
+            "Ebork",
+            "Astram",
+            "Elian",
+            "Tarlock",
+            "Grog",
+            "Myra",
+            "Sullivan",
+            "Marlo",
+            "Jax",
+            "Anthony",
+            "Bhurdan",
+            "Thoreuth",
+            "Bob",
+            "Varis",
+            "Nyx",
+            "Luna",
+            "Ash",
+            "Rhogar",
+            "Ember",
+            "Mikel",
+            "Yamil",
+            "Velithria",
+        ]
+        self.character_name = (
+            f"{character_names[gamertag%len(character_names)]}#{gamertag}"
+        )
+
+    def __eq__(self, other):
+        return self.gamertag == other.gamertag
+
+    def __lt__(self, other):
+        return self.gamertag < other.gamertag
+
+    def __gt__(self, other):
+        return self.gamertag > other.gamertag
+
+    def __repr__(self):
+        return "".join(self.character_name)
+
+
+def print_tree(bst_node, lines, level=0):
+    if bst_node != None:
+        print_tree(bst_node.right, lines, level + 1)
+        lines.append(" " * 4 * level + "> " + str(bst_node.val))
+        print_tree(bst_node.left, lines, level + 1)
+
+
+def get_characters(num):
+    random.seed(1)
+    characters = []
+    for _ in range(num):
+        character = Character(random.randint(0, num - 1))
+        characters.append(character)
+    return characters
+
+
+def test(num_characters):
+    characters = get_characters(num_characters)
+    bst = BSTNode()
+    for character in characters:
+        print(f"Inserting {character} into tree...")
+        bst.insert(character)
+    print("Tree:")
+    print("-------------------------------------")
+    print(bst)
+    print("=====================================")
+
+
+def main():
+    test(3)
+    test(5)
+    test(10)
+
+
+main()
