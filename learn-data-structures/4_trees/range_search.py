f4ef625046c7bd701c16680227ee3374b3e92e47
avearmin <117543612+avearmin@users.noreply.github.com>
2023-06-10 20:47:38 -0400

    Add starter code for range search exercise
    Course: Learn Data Structures
    Chapter: 4. Trees
    Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/36c4edda-52be-4679-a7d4-8fd81c7f7bdb/386b4010-be66-4790-8eb0-1bdf485dd5df
    
    We are to complete the `search_range` method for the `BSTNode` class. 
    The method should take two arguments, lower_bound and upper_bound, 
    and return a list of character objects whose gamer tags fall within the
     specified range, inclusive.

---
+++ learn-data-structures/4_trees/range_search.py
@@ -0,0 +1,137 @@
+# exercise link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/36c4edda-52be-4679-a7d4-8fd81c7f7bdb/386b4010-be66-4790-8eb0-1bdf485dd5df
+
+import random
+
+
+class BSTNode:
+    def search_range(self, lower_bound, upper_bound):
+        # ?
+
+    # don't touch below this line
+
+    def exists(self, val):
+        if val == self.val:
+            return True
+
+        if val < self.val:
+            if self.left == None:
+                return False
+            return self.left.exists(val)
+
+        if self.right == None:
+            return False
+        return self.right.exists(val)
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
+    def __repr__(self):
+        lines = []
+        print_tree(self, lines)
+        return "\n".join(lines)
+
+
+class Character:
+    def __init__(self, gamertag):
+        self.gamertag = gamertag
+        character_names = [
+            "Ebork",
+            "Astram",
+            "Elian",
+            "Tarlock",
+            "Grog",
+            "Myra",
+            "Sullivan",
+            "Marlo",
+            "Jax",
+            "Anthony",
+            "Bhurdan",
+            "Thoreuth",
+            "Bob",
+            "Varis",
+            "Nyx",
+            "Luna",
+            "Ash",
+            "Rhogar",
+            "Ember",
+            "Mikel",
+            "Yamil",
+            "Velithria",
+        ]
+        self.character_name = (
+            f"{character_names[gamertag%len(character_names)]}#{gamertag}"
+        )
+
+    def __eq__(self, other):
+        return self.gamertag == other.gamertag
+
+    def __lt__(self, other):
+        return self.gamertag < other.gamertag
+
+    def __gt__(self, other):
+        return self.gamertag > other.gamertag
+
+    def __repr__(self):
+        return "".join(self.character_name)
+
+
+def print_tree(bst_node, lines, level=0):
+    if bst_node != None:
+        print_tree(bst_node.right, lines, level + 1)
+        lines.append(" " * 4 * level + "> " + str(bst_node.val))
+        print_tree(bst_node.left, lines, level + 1)
+
+
+def get_characters(num):
+    characters = []
+    for _ in range(num):
+        character = Character(random.randint(0, num - 1))
+        characters.append(character)
+    return characters
+
+
+def test(num_characters, lower_bound, upper_bound):
+    characters = get_characters(num_characters)
+    bst = BSTNode()
+    for character in characters:
+        bst.insert(character)
+    print("Tree:")
+    print(bst)
+    print("-------------------------------------")
+    print(f"Searching for characters between {lower_bound} and {upper_bound}:")
+    result = bst.search_range(lower_bound, upper_bound)
+    for character in sorted(result):
+        print(f" - {character}")
+    print("=====================================")
+
+
+def main():
+    random.seed(1)
+    test(10, 2, 6)
+    test(20, 8, 14)
+    test(30, 12, 24)
+
+
+main()
