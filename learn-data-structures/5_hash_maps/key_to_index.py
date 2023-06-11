be0c0e52c47dd48c05d9d7167cfb7d6a6b0c2383
avearmin <117543612+avearmin@users.noreply.github.com>
2023-06-11 18:48:56 -0400

    Add starter code for key to index exercise
    Course: Learn Data Structures
    Chapter: 5. Hash Maps
    Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/d698f2b9-8526-4a11-9e8c-ad45890bd220/27eef24f-7b9a-41b2-81e1-d84e13eb919a
    
    In the next set of exercises we'll be building a hash map to gain an
    understanding of how Python dictionaries work. This exercise asks us
    to complete the hashing function which is called `key_to_index`. The 
    function should:
    
    -Take a key (string) as input
    -Add the ASCII values of all the characters in the string
    -Mod ( % ) the sum by the size of the hashtable to get an index which should 
      be an int
    -Return the index
    
    Additionally, I have created a new directory `5_hash_maps` to hold these exercises.

---
+++ learn-data-structures/5_hash_maps/key_to_index.py
@@ -0,0 +1,35 @@
+# exercise link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/d698f2b9-8526-4a11-9e8c-ad45890bd220/27eef24f-7b9a-41b2-81e1-d84e13eb919a
+
+class HashMap:
+    def key_to_index(self, key):
+        # ?
+
+    # don't touch below this line
+
+    def __init__(self, size):
+        self.hashmap = [None for i in range(size)]
+
+    def __repr__(self):
+        buckets = []
+        for v in self.hashmap:
+            if v != None:
+                buckets.append(v)
+        return str(buckets)
+
+
+def test(size, keys):
+    hm = HashMap(size)
+    print(f"Using hashmap with size: {size}")
+    for key in keys:
+        index = hm.key_to_index(key)
+        print(f"{key} hashes to index {index}")
+    print("=====================================")
+
+
+def main():
+    test(4, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
+    test(256, ["hello", "world"])
+    test(512, ["golang", "python", "java", "javascript", "rust", "c", "c++"])
+
+
+main()
