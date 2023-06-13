ab53ce232aff60f745c4d4c12c9af41d699eae7b
Armin Radoncic <Arminradoncic1@gmail.com>
2023-06-13 18:53:40 -0400

    Add starter code for tries exists exercise
    Course: Learn Data Structures
    Chapter: 6. Tries
    Exercise Link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/61e9d21f-63e5-4e9f-b294-4c1972d043ab/aa5dc9d4-5286-4adf-a8c5-03e738a781a6
    
    In this exercise, we are to complete the `exists` method which takes a
    word as an input, and should return True if the it exists in the trie and
    False if not.

---
+++ learn-data-structures/6_tries/tries_exists.py
@@ -0,0 +1,43 @@
+# exercise link: https://boot.dev/course/7bbb53ed-2106-4f6b-b885-e7645c2ff9d8/61e9d21f-63e5-4e9f-b294-4c1972d043ab/aa5dc9d4-5286-4adf-a8c5-03e738a781a6
+
+import json
+
+
+class Trie:
+    def exists(self, word):
+        # ?
+
+    # don't touch below this line
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+
+def test(words, words_to_check):
+    trie = Trie()
+    for word in words:
+        trie.add(word)
+    print("Trie:")
+    print(json.dumps(trie.root, sort_keys=True, indent=2))
+    print("-------------------------------------")
+    for word in words_to_check:
+        exists = trie.exists(word)
+        print(f"'{word}' exists: {exists}")
+    print("=====================================")
+
+
+def main():
+    test(["be", "bad", "back", "bat"], ["bad", "notfound"])
+    test(["a", "to", "tea", "ted", "ten", "i", "in", "inn"], ["ted", "ten", "notfound"])
+
+
+main()
